1d1af60e0687584359a4d305a3a7c5ff13a4045f
nadiaolga <nadiaolga@yandex.ru>
2026-05-17 18:01:00 +0300

    task_02

--- 05.py
+++ 05.py
@@ -8,6 +8,19 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    # Студент выставляет оценку лектору.
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if isinstance(grade, int) and grade <= 10 and grade >= 0:
+                if course in lecturer.grades:
+                    lecturer.grades[course] += [grade]
+                else:
+                    lecturer.grades[course] = [grade]
+            else:
+                return 'Оценка неправильная'
+        else:
+            return 'Ошибка'
+
 
 # Класс преподавателей. Имя, фамилия и список закрепленных курсов.
 class Mentor:
@@ -15,6 +28,19 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+# Дочерний класс Lecturer (лекторы).
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+# Дочерний класс Reviewer (эксперты, проверяющие домашние задания).
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -26,33 +52,35 @@ class Mentor:
             return 'Ошибка'
 
 
-# Дочерний класс Lecturer (лекторы).
-class Lecturer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-
-
-# Дочерний класс Reviewer (эксперты, проверяющие домашние задания).
-class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
 
 # print(best_student.grades)
 
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
-print(isinstance(lecturer, Mentor)) # True
-print(isinstance(reviewer, Mentor)) # True
-print(lecturer.courses_attached)    # []
-print(reviewer.courses_attached)    # []
+# print(isinstance(lecturer, Mentor)) # True
+# print(isinstance(reviewer, Mentor)) # True
+# print(lecturer.courses_attached)    # []
+# print(reviewer.courses_attached)    # []
+
+student = Student('Алёхина', 'Ольга', 'Ж')
+
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+print(student.rate_lecture(lecturer, 'Python', 7))   # None
+print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
+print(student.rate_lecture(lecturer, 'С++', 8))      # Ошибка
+print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
+
+print(lecturer.grades)  # {'Python': [7]}
+
